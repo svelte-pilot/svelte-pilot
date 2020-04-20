4ecf9542d53c2099546e5370ce28836a5a97a5bf
Jiang Fengming <jiangfengming87@gmail.com>
2020-04-20 10:54:36 +0800

    tmp

--- src/index.mjs
+++
@@ -1,4 +0,0 @@
-export { default as PathRouter } from './PathRouter'
-export { default as HashRouter } from './HashRouter'
-export { default as RouterView } from './RouterView'
-export { default as RouterLink } from './RouterLink'
