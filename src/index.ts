9ccc6fb10b26f21b6e7ccae78ac962a2ad720536
fenix.sol <jiangfengming87@gmail.com>
2023-10-10 12:00:32 +0800

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,22 @@
-import _ServerApp from './ServerApp.svelte'
+import _ServerApp from "./ServerApp.svelte";
 
 type ServerSideComponent = {
   render: (
     props?: Record<string, unknown>,
     { context }?: { context?: Map<unknown, unknown> }
   ) => {
-    html: string
-    css: { code: string; map: string }
-    head: string
-  }
-}
+    html: string;
+    css: { code: string; map: string };
+    head: string;
+  };
+};
 
-export { default as ClientApp } from './ClientApp.svelte'
-export { default as Link } from './Link.svelte'
-export * from './Router'
-export { default as Router } from './Router'
-export { default as View } from './View.svelte'
-export const ServerApp = (<unknown>_ServerApp) as ServerSideComponent
+export { default as ClientApp } from "./ClientApp.svelte";
+export {
+  default as Link,
+  setDefaultMethod as setLinkMethod,
+} from "./Link.svelte";
+export * from "./Router";
+export { default as Router } from "./Router";
+export { default as View } from "./View.svelte";
+export const ServerApp = (<unknown>_ServerApp) as ServerSideComponent;
